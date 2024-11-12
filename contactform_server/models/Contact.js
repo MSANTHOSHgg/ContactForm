363fcf5660f0058c12ebf033fa7baac7eaf115cf
Santhosh <msandycrime@gmail.com>
2024-11-12 14:44:28 +0530

    Completed

--- contactform_server/models/Contact.js
+++ contactform_server/models/Contact.js
@@ -4,6 +4,7 @@ const ContactSchema = new mongoose.Schema({
     name: String,
     email: String,
     message: String,
+    date: { type: Date, default: Date.now }
 });
 
 const ContactModel = mongoose.model('contactforms', ContactSchema);
